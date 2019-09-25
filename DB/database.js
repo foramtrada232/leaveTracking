d44c6df4b09e928b643cde45079ef527914c1541
foramtrada232 <foramtrada232@gmail.com>
2019-09-25 17:55:36 +0530

    done

--- DB/database.js
+++ DB/database.js
@@ -1,1 +1,31 @@
-console.log("=======================>",this.state.searchedProject.projectData)
+const mongoose = require("mongoose");
+
+const db = function () {
+    let initFlag = false;
+    return {
+        config(addr, dbname, dbUsername, dbPassword, opts, callback) {
+            if (!initFlag) {
+                const connectUrl = `mongodb://3.8.156.162:27017/leaveTracking`;
+                mongoose.connect(connectUrl, (opts || {}));
+
+                const dbConnection = mongoose.connection;
+
+                dbConnection.on("error", (err) => {
+                    // Connection Error
+                    console.log(`Mongodb error encountered [${err}]`);
+
+                    if (callback) {
+                        callback("ERR-MONGODB", `mongodb - ${err.message}`);
+                    }
+                });
+
+                dbConnection.once("open", () => {
+                    initFlag = true;
+                    if (callback) callback(null);
+                });
+            } else if (callback) callback(null);
+        },
+    };
+};
+
+module.exports = db();
